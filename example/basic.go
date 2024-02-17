21732aa87453dccd7b0f657acc6ed408e7dbae24
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-02-17 21:17:27 +0530

    feat: add basic example usage of govents

---
+++ example/basic.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/ghoshRitesh12/govents"
+)
+
+func main() {
+	sample := govents.NewEventEmitter[int]()
+	strs := []string{}
+
+	sample.On("data", func(vals ...int) {
+		for _, val := range vals {
+			strs = append(strs, fmt.Sprintf("v%d", val))
+		}
+		fmt.Println("inside eventListener", strs)
+	})
+
+	fmt.Println("outside eventListener", strs)
+
+	for range 4 {
+		time.Sleep(time.Second)
+		sample.Emit("data", rand.Intn(21), rand.Intn(21), rand.Intn(21))
+	}
+}
